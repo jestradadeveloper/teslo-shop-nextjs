30fb07a3612a3e6be23b943c65eb53527490fdcf
Jose Estrada <j.estrada.developer@gmail.com>
2022-04-19 13:28:02 -0500

    Add ShopLayout and initial public resources data

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,12 @@
+import { Typography } from '@mui/material';
 import type { NextPage } from 'next';
+import { ShopLayout } from '../components/layouts';
 const Home: NextPage = () => {
   return (
-   <h1>Teslo Shop</h1>
+   <ShopLayout title={'Tesla Shop - Home'} pageDescription={'Find best Tesla products'}>
+     <Typography variant='h1' component='h1'>Tienda</Typography>
+     <Typography variant='h2'  sx={{ mb: 1 }}>Todos los productos</Typography>
+   </ShopLayout>
   )
 }
 
