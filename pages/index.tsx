4ef5ef6987b47e056e9fe37745e501d519901859
Jose Estrada <j.estrada.developer@gmail.com>
2022-04-20 15:20:00 -0500

    Add Provitional Home product cards

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,30 @@
-import { Typography } from '@mui/material';
+import { Card, CardActionArea, CardMedia, Grid, Typography } from '@mui/material';
 import type { NextPage } from 'next';
 import { ShopLayout } from '../components/layouts';
+import { initialData } from '../database/products';
+
+
 const Home: NextPage = () => {
   return (
    <ShopLayout title={'Tesla Shop - Home'} pageDescription={'Find best Tesla products'}>
      <Typography variant='h1' component='h1'>Tienda</Typography>
      <Typography variant='h2'  sx={{ mb: 1 }}>Todos los productos</Typography>
+     <Grid container spacing={4} >
+       {initialData.products.map(product=>(
+        <Grid item xs={6} sm={4} key={product.slug}>
+          <Card>
+            <CardActionArea>
+              <CardMedia 
+                component='img'
+                image={`products/${product.images[0]}`}
+                alt={product.title}
+              />
+            </CardActionArea>
+          </Card>
+        </Grid>
+       ))}
+        
+     </Grid>
    </ShopLayout>
   )
 }
