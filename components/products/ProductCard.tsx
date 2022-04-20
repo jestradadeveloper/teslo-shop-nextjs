3339d0f5471a464d15441d6ef211a7e9246ca23d
Jose Estrada <j.estrada.developer@gmail.com>
2022-04-20 17:35:12 -0500

    Add product card extra info and hover image effect

--- components/products/ProductCard.tsx
+++ components/products/ProductCard.tsx
@@ -1,22 +1,39 @@
-import { Card, CardActionArea, CardMedia, Grid } from '@mui/material'
-import { FC } from 'react'
+import { Box, Card, CardActionArea, CardMedia, Grid, Typography } from '@mui/material'
+import { FC, useMemo, useState } from 'react'
 import { IProduct } from '../../interfaces'
 
 interface Props{
   product: IProduct;
 }
 export const ProductCard:FC<Props> = ({ product }) => {
+
+  const [isHovered, setIsHovered] = useState(false);
+  const productImage = useMemo(()=>{
+    return isHovered 
+    ? `products/${product.images[1]}`
+    : `products/${product.images[0]}`
+  }, [isHovered, product.images])
   return (
-    <Grid item xs={6} sm={4} key={product.slug}>
+    <Grid 
+      item 
+      xs={6} sm={4} 
+      onMouseEnter={()=> setIsHovered(true) }
+      onMouseLeave={()=> setIsHovered(false) }
+    >
       <Card>
         <CardActionArea>
           <CardMedia 
             component='img'
-            image={`products/${product.images[0]}`}
+            className='fadeIn'
+            image={ productImage }
             alt={product.title}
           />
         </CardActionArea>
       </Card>
+      <Box sx={{mt:1}} className='fadeIn'>
+        <Typography fontWeight={800}>{ product.title }</Typography>
+        <Typography fontWeight={600}>{ `$${product.price}` }</Typography>
+      </Box>
   </Grid>
   )
 }
