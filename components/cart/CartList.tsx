8165c3a783bfe2e1559453e95894075ad01c0256
Jose Estrada <j.estrada.developer@gmail.com>
2022-04-22 10:50:35 -0500

    Add Order Summary

--- components/cart/CartList.tsx
+++ components/cart/CartList.tsx
@@ -10,10 +10,10 @@ const productsInCart = [
   initialData.products[2]
 ]
 interface Props{
-  editable: boolean;
+  editable?: boolean;
 }
 
-export const CartList:FC<Props> = ({editable}) => {
+export const CartList:FC<Props> = ({editable = false}) => {
   return (
     <>
       {
@@ -24,7 +24,7 @@ export const CartList:FC<Props> = ({editable}) => {
                 <Link>
                   <CardActionArea>
                     <CardMedia
-                      image={`products/${product.images[0]}`}
+                      image={`/products/${product.images[0]}`}
                       component='img'
                       sx={{ borderRadius: '5px' }}
                     />
