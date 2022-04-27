133f5457d940e4881183ac618f63d895ddd21ad4
Jose Estrada <j.estrada.developer@gmail.com>
2022-04-27 16:11:28 -0500

    Update IProduct Interface with timestamp fields

--- interfaces/products.ts
+++ interfaces/products.ts
@@ -9,7 +9,9 @@ export interface IProduct {
   tags: string[];
   title: string;
   type: IValidType;
-  gender: 'men'|'women'|'kid'|'unisex'
+  gender: 'men'|'women'|'kid'|'unisex';
+  createdAt: string;
+  updatedAt: string;
 }
 
 export type IValidSize = 'XS'|'S'|'M'|'L'|'XL'|'XXL'|'XXXL';
