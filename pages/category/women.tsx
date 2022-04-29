7cba9ce0cb63c424f3b7cf58c575a29c138e6ab5
Jose Estrada <j.estrada.developer@gmail.com>
2022-04-29 17:22:27 -0500

    Add Category Women Page

---
+++ pages/category/women.tsx
@@ -0,0 +1,25 @@
+import { Grid, Typography } from '@mui/material';
+import { ShopLayout } from '../../components/layouts'
+import { ProductList } from '../../components/products';
+import { FullScreenLoading } from '../../components/ui';
+import { useProducts } from '../../hooks';
+
+const WomenCategoryPage = () => {
+  const { products, isLoading } = useProducts('/products?gender=women');
+  return (
+    <ShopLayout title='Category for Women' pageDescription='Find Products for women on TesloShop'>
+      <Grid container spacing={3} display='flex' justifyContent='center' sx={{paddingY: 12}}>
+        <Typography component='h1' variant='h1'>Women  | Todos los productos </Typography>
+      </Grid>
+      {
+       isLoading
+       ?
+       <FullScreenLoading />
+       :
+       <ProductList products={ products } />
+      }
+    </ShopLayout>
+  )
+}
+
+export default WomenCategoryPage
