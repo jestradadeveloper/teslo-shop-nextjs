cbbb7e06749ccf2b02d5dd4326ef55b67a49e6cd
Jose Estrada <j.estrada.developer@gmail.com>
2022-04-29 15:37:07 -0500

    Add useProducts with useSWR

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,19 +2,22 @@ import { Card, CardActionArea, CardMedia, Grid, Typography } from '@mui/material
 import type { NextPage } from 'next';
 import { ShopLayout } from '../components/layouts';
 import { ProductList } from '../components/products';
-import { initialData } from '../database/products';
+import { useProducts } from '../hooks/useProducts';
 
-import useSWR from 'swr';
-const fetcher = (...args: [key: string]) => fetch(...args).then(res => res.json())
 const HomePage: NextPage = () => {
-  const { data, error } = useSWR('/api/products', fetcher)
-  if (error) return <div>failed to load</div>
-  if (!data) return <div>loading...</div>
+  const { products, isLoading } = useProducts('/products');
   return (
    <ShopLayout title={'Tesla Shop - Home'} pageDescription={'Find best Tesla products'}>
      <Typography variant='h1' component='h1'>Tienda</Typography>
      <Typography variant='h2'  sx={{ mb: 1 }}>Todos los productos</Typography>
-     <ProductList products={ data } />
+     {
+       isLoading
+       ?
+       <h1>Loading</h1>
+       :
+       <ProductList products={ products } />
+     }
+    
    </ShopLayout>
   )
 }
