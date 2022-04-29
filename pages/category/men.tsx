bd37c480b512f9526b2302f4589a4db91ca352a9
Jose Estrada <j.estrada.developer@gmail.com>
2022-04-29 17:22:13 -0500

    Add Category men Page

---
+++ pages/category/men.tsx
@@ -0,0 +1,25 @@
+import { Grid, Typography } from '@mui/material';
+import { ShopLayout } from '../../components/layouts'
+import { ProductList } from '../../components/products';
+import { FullScreenLoading } from '../../components/ui';
+import { useProducts } from '../../hooks';
+
+const MenCategoryPage = () => {
+  const { products, isLoading } = useProducts('/products?gender=men');
+  return (
+    <ShopLayout title='Category for Men' pageDescription='Products for men on TesloShop'>
+      <Grid container spacing={3} display='flex' justifyContent='center' sx={{paddingY: 12}}>
+        <Typography component='h1' variant='h1'>Men | Todos los productos</Typography>
+      </Grid>
+      {
+       isLoading
+       ?
+       <FullScreenLoading />
+       :
+       <ProductList products={ products } />
+     }
+    </ShopLayout>
+  )
+}
+
+export default MenCategoryPage
