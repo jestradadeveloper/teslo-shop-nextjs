5cbc59ee7588cb0e6d522c20f0a2ba599f5cd272
Jose Estrada <j.estrada.developer@gmail.com>
2022-04-29 13:44:17 -0500

    add swr custom hook to fetch database info to home

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,15 +4,19 @@ import { ShopLayout } from '../components/layouts';
 import { ProductList } from '../components/products';
 import { initialData } from '../database/products';
 
-
-const Home: NextPage = () => {
+import useSWR from 'swr';
+const fetcher = (...args: [key: string]) => fetch(...args).then(res => res.json())
+const HomePage: NextPage = () => {
+  const { data, error } = useSWR('/api/products', fetcher)
+  if (error) return <div>failed to load</div>
+  if (!data) return <div>loading...</div>
   return (
    <ShopLayout title={'Tesla Shop - Home'} pageDescription={'Find best Tesla products'}>
      <Typography variant='h1' component='h1'>Tienda</Typography>
      <Typography variant='h2'  sx={{ mb: 1 }}>Todos los productos</Typography>
-     <ProductList products={ initialData.products as any} />
+     <ProductList products={ data } />
    </ShopLayout>
   )
 }
 
-export default Home
+export default HomePage
