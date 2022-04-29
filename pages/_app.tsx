0da6ab3b657de9e50ebd6a8b0d43c1d04e2c5bd8
Jose Estrada <j.estrada.developer@gmail.com>
2022-04-29 15:44:45 -0500

    add global config fetcher for useProducts on _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,25 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { SWRConfig } from 'swr';
 import { CssBaseline, ThemeProvider } from '@mui/material';
 import { lightTheme } from '../themes/light-theme';
 
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
+    <SWRConfig  
+      value={
+        {
+          fetcher: (resource, init) => fetch(resource, init).then( res=> res.json() )
+        }
+      }
+
+    >   
     <ThemeProvider theme={ lightTheme }>
       <CssBaseline />
       <Component {...pageProps} />
-    </ThemeProvider>
+    </ThemeProvider> 
+  </SWRConfig>
   )
 }
 
