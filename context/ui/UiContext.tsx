63cf8583582e2670b2a18474589c4de931bb5e36
Jose Estrada <j.estrada.developer@gmail.com>
2022-05-11 15:04:25 -0500

    Add UiContext file function
    Add UiContext file to index bareel file with props and function isMenuOpen and toggleSideMenu

---
+++ context/ui/UiContext.tsx
@@ -0,0 +1,10 @@
+import { createContext } from 'react';
+
+
+export interface ContextProps{ 
+   isMenuOpen: boolean;
+   toggleSideMenu: () => void;
+}
+
+
+export const UiContext = createContext({} as ContextProps);
