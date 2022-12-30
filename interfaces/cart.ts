477c550276917407c7938ca41351ad753a2ad2ac
Jose Estrada <j.estrada.developer@gmail.com>
2022-12-29 22:30:47 -0600

    Add ICartProduct Interface

---
+++ interfaces/cart.ts
@@ -0,0 +1,12 @@
+import { IValidSize } from './';
+
+export interface ICartProduct {
+  _id: string;
+  image: string;
+  price: number;
+  size: IValidSize;
+  slug: string;
+  title: string;
+  gender: 'men' | 'women' | 'kid' | 'unisex';
+  quantity: number;
+}
