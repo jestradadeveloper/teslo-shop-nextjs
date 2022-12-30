83f0a752ed8faf9fcf015257fa6e349eacffd94f
Jose Estrada <j.estrada.developer@gmail.com>
2022-12-29 22:31:55 -0600

    Add CartProvider

---
+++ context/cart/CartProvider.tsx
@@ -0,0 +1,23 @@
+import { FC, useReducer } from 'react'; 
+import { ICartProduct } from '../../interfaces';
+import { CartContext, cartReducer } from './'; 
+
+export interface CartState {
+ cart: ICartProduct[]
+}
+
+const CART_INITIAL_STATE: CartState = {
+  cart: []
+}
+interface LayoutProps {
+  children: React.ReactNode;
+}
+
+export const CartProvider = ({ children }:LayoutProps) => {
+  const [state, dispatch] = useReducer(cartReducer, CART_INITIAL_STATE);
+  return(
+    <CartContext.Provider value={{...state}}>
+        {children}
+    </CartContext.Provider>
+  )
+}
